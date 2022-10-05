2df52eb29da6af9164c5d20016d1e32d14e646b2
Okoro John <okorojohndarlington@gmail.com>
2022-10-05 07:59:21 +0100

    Updated

--- pages/api/profile/index.js
+++ pages/api/profile/index.js
@@ -1,7 +1,5 @@
 import dbConnect from "../../../config/mongo";
 import Order from "../../../models/Order";
-import User from "../../../models/User";
-import { isVerified } from "../../../middleware/authMiddleware";
 
 export default async function handler(req, res) {
 	const { method } = req;
@@ -9,16 +7,11 @@ export default async function handler(req, res) {
 
 	// Get my order
 	if (method === "GET") {
-		const user = await User.findById(req.user._id);
-		if (isVerified) {
-			try {
-				const orders = await Order.find({ userId: user._id });
-				res.status(201).json(orders);
-			} catch (err) {
-				res.status(500).json(err);
-			}
-		} else {
-			res.status(404).json("You are not authorized to access this route");
+		try {
+			const orders = await Order.find({ userId: user._id });
+			res.status(201).json(orders);
+		} catch (err) {
+			res.status(500).json(err);
 		}
 	}
 
