c7704ec25103ad20c8f3b55392c663826313eccc
Okoro John <okorojohndarlington@gmail.com>
2022-10-13 13:59:03 +0100

    Updated

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-	baseURL: "https://next-food-ordering-app-flame.vercel.app/api",
+	baseURL: "http://localhost:3000/api",
 });
 
 export default axiosInstance;
